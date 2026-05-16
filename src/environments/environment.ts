512027d94f3203cf75239ea11260810ea02f14ad
Usman Zia <uzia@infinityels.com>
2026-05-16 20:59:26 +0500

    Use live API URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:5001/api',
+  apiUrl: 'http://nextflowtech.runasp.net/api',
   externalAuth: {
     googleClientId: '',
     microsoftClientId: '',
